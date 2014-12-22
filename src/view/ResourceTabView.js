18cef93f74b456d5f1eb79a985b870f86a06d0ed
Mikael Lindqvist <li.mikael@gmail.com>
2014-12-22 08:08:50 +0100

    progress

--- src/view/ResourceTabView.js
+++ src/view/ResourceTabView.js
@@ -1,17 +1,58 @@
 var xnode = require("xnode");
 var inherits = require("inherits");
+var xnodeui = require("xnodeui");
 
 function ResourceTabView() {
 	xnode.Div.call(this);
+
+//	this.innerHTML="hello";
+
+/*	this.container=new xnode.Div();
+	this.container.style.position = "absolute";
+	this.container.style.left = 10;
+	this.container.style.right = 10;
+	this.container.style.top = 60;
+	this.container.style.bottom = 10;
+	this.container.style.background="#ff0000";
+	this.container.innerHTML="hello";
+
+	this.appendChild(this.container);
+
+	this.accordion = new xnodeui.Accordion();
+	this.accordion.style.position = "absolute";
+	this.accordion.style.left = 5;
+	this.accordion.style.right = 5;
+	this.accordion.style.top = 5;
+	this.accordion.style.bottom = 5;
+
+	this.accordion.appendChild(new xnode.Div("hello"));
+	this.accordion.appendChild(new xnode.Div("some content...<br/>blalabl"));
+	this.accordion.appendChild(new xnode.Div("hello 2"));
+	this.accordion.appendChild(new xnode.Div("some more content...<br/>blalabl and so on...<br/>blalabl and so on...<br/>blalabl and so on...<br/>"));
+
+
+	this.accordion.heightStyle = "fill";
+	this.accordion.collapsible = false;
+	this.accordion.autoHeight = false;
+
+	this.container.appendChild(this.accordion);
+
+	var scope = this;
+
+	setTimeout(function() {
+		scope.accordion.heightStyle = "fill";
+		scope.accordion.collapsible = false;
+		scope.accordion.refresh();
+	}, 0);*/
 }
 
 inherits(ResourceTabView, xnode.Div);
 
 ResourceTabView.prototype.setData = function(data) {
 	if (data) {
-		this.id = data.id;
+			this.id = data.id;
 
-		this.innerHTML = "hello world: " + data.label;
+			//this.innerHTML = "hello world: " + data.label;
 	}
 }
 
